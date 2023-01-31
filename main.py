7358f1ace1510de82166f3471470cd18ac652e7f
jucove <74031526@continental.edu.pe>
2023-01-31 10:08:37 -0500

    Implementacion en main.py del codigo para ingresar y calcular el MCD de N numeros

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+def mcd(a, b):
+    r = a % b if b !=0 else a
+    return b if r == 0 else mcd(b, r)
 
-#Escribir el codigo de main.py para ingresar n numeros naturales y calcular el MCD de los numeros ingresados.
+numeros = float( input ("Ingrese la cantidad de numeros a calcular el MCD: "))
 
-if __name__ == '__main__':
-    print('En desarrollo')
+def f(numbers):
+    if len(numbers) >=2:
+        r = mcd(numbers[0], numbers[1])
+        for n in numbers[2:]:
+            r = mcd(r, n)
+        return r
+numbers = []
+i = 1
+while i <= numeros:
+    try:
+        num = int(input("Ingrese el {}° numero : ".format(i)))
+        numbers.append(num)
+        i += 1
+    except ValueError as e:
+        print("El valor ingresado no es un numero valido")
+
+result = f(numbers)
+print('El maximo comun divisor es: ')
+print(result)
