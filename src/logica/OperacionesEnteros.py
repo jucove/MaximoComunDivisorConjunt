d26ae9ac89874c2d54b97c4bf9027751a475ea23
jucove <74031526@continental.edu.pe>
2023-01-31 08:49:11 -0500

    mcd con 3 numeros (verde)

--- src/logica/OperacionesEnteros.py
+++ src/logica/OperacionesEnteros.py
@@ -1,2 +1,22 @@
 class OperacionesEnteros:
-    pass
+    def __init__(self, numeros):
+        self.__numeros = numeros
+
+    def calcularMCD(self):
+        if len(self.__numeros) == 2 :
+            return self.MCD(self.__numeros[0], self.__numeros[1])
+        elif len(self.__numeros) == 3 :
+            mcd = self.MCD(self.__numeros[0], self.__numeros[1])
+            mcd = self.MCD(mcd, self.__numeros[2])
+
+            return mcd
+        else:
+            return 0
+
+    def MCD(self, numero1, numero2):
+        temporal = 0
+        while numero2 != 0:
+            temporal = numero2
+            numero2 = numero1 % numero2
+            numero1 = temporal
+        return numero1
--- tests/PruebaOperacionesEnteros.py
+++ tests/PruebaOperacionesEnteros.py
@@ -1,4 +1,29 @@
 import unittest
-
+from src.logica.OperacionesEnteros import OperacionesEnteros
 class PruebaOperacionesEteros(unittest.TestCase):
-    pass
+    def test_MCD_dosNumerosPositivos_retornaMCD(self):
+        # Arrange
+        numero1 = 18
+        numero2 = 24
+        resultadoEsperado = 6
+        operacion = OperacionesEnteros([numero1, numero2])
+
+        # Do
+        resultadoActual = operacion.calcularMCD()
+
+        # Assert
+        self.assertEqual(resultadoEsperado, resultadoActual)
+
+    def test_MCD_tresNumerosPositivos_retornaMCD(self):
+        # Arrange
+        numero1 = 18
+        numero2 = 24
+        numero3 = 30
+        resultadoEsperado = 6
+        operacion = OperacionesEnteros([numero1, numero2, numero3])
+
+        # Do
+        resultadoActual = operacion.calcularMCD()
+
+        # Assert
+        self.assertEqual(resultadoEsperado, resultadoActual)
