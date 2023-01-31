8fde1a195911d93a50aeb5a5ba6b0ff9b1e0c377
jucove <74031526@continental.edu.pe>
2023-01-31 08:55:48 -0500

    mcd con 3 numeros-refactorizacion (azul)

--- src/logica/OperacionesEnteros.py
+++ src/logica/OperacionesEnteros.py
@@ -3,13 +3,8 @@ class OperacionesEnteros:
         self.__numeros = numeros
 
     def calcularMCD(self):
-        if len(self.__numeros) == 2 :
-            return self.MCD(self.__numeros[0], self.__numeros[1])
-        elif len(self.__numeros) == 3 :
-            mcd = self.MCD(self.__numeros[0], self.__numeros[1])
-            mcd = self.MCD(mcd, self.__numeros[2])
-
-            return mcd
+        if len(self.__numeros) > 1 :
+            return self.MCDMasDosNumeros()
         else:
             return 0
 
@@ -20,3 +15,13 @@ class OperacionesEnteros:
             numero2 = numero1 % numero2
             numero1 = temporal
         return numero1
+
+    def MCDMasDosNumeros(self):
+        cantidadNumeros = len(self.__numeros)
+        mcd = self.MCD(self.__numeros[0], self.__numeros[1])
+        i = 0
+        while (i < (cantidadNumeros - 2)):
+            mcd = self.MCD(mcd, self.__numeros[i + 2])
+            i = i + 1
+        return mcd
+
